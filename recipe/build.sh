4fdb152a1e2b888d905b00fb38671e0b355631b1
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-25 16:16:32 -0700

    Update for ASP 3.5.0
    Sync up

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,10 +11,21 @@ cd build
 # Recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
 find ${PREFIX}/lib -name '*.la' -delete
 
-cmake ..                             \
-    -DCMAKE_PREFIX_PATH=${PREFIX}    \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DASP_DEPS_DIR=${PREFIX}         \
+if [ "$(uname)" = "Darwin" ]; then
+    cc_comp=clang
+    cxx_comp=clang++
+else
+    cc_comp=x86_64-conda-linux-gnu-gcc
+    cxx_comp=x86_64-conda-linux-gnu-c++
+fi
+
+# Enforce a compiler we know to work
+cmake ..                                         \
+    -DCMAKE_C_COMPILER=${PREFIX}/bin/$cc_comp    \
+    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/$cxx_comp \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
+    -DASP_DEPS_DIR=${PREFIX}                     \
     -DCMAKE_VERBOSE_MAKEFILE=ON
 
 make -j${CPU_COUNT}
