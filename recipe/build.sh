a9f73b20e10e2e5f9a1b23462bf0abecac212e06
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-04-25 20:23:47 -0700

    Use known good compiler, but in compiler section

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,18 +11,18 @@ cd build
 # Recommended in https://gitter.im/conda-forge/conda-forge.github.io?at=5c40da7f95e17b45256960ce
 find ${PREFIX}/lib -name '*.la' -delete
 
-if [ "$(uname)" = "Darwin" ]; then
-    cc_comp=clang
-    cxx_comp=clang++
-else
-    cc_comp=x86_64-conda-linux-gnu-gcc
-    cxx_comp=x86_64-conda-linux-gnu-c++
-fi
+# if [ "$(uname)" = "Darwin" ]; then
+#     cc_comp=clang
+#     cxx_comp=clang++
+# else
+#     cc_comp=x86_64-conda-linux-gnu-gcc
+#     cxx_comp=x86_64-conda-linux-gnu-c++
+# fi
 
-# Enforce a compiler we know to work
+    # -DCMAKE_C_COMPILER=${PREFIX}/bin/$cc_comp    \
+    # -DCMAKE_CXX_COMPILER=${PREFIX}/bin/$cxx_comp \
+
 cmake ..                                         \
-    -DCMAKE_C_COMPILER=${PREFIX}/bin/$cc_comp    \
-    -DCMAKE_CXX_COMPILER=${PREFIX}/bin/$cxx_comp \
     -DCMAKE_PREFIX_PATH=${PREFIX}                \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}             \
     -DASP_DEPS_DIR=${PREFIX}                     \
